9f5da54ee8a09ef05b9f5e6a9bc3ba601ccf4f30
KonstantinKhodot <Khodot.konstantin@mail.ru>
2022-06-02 14:49:14 +0300

    False commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -39,7 +39,7 @@ public class BonusServiceTest {
 
         // подготавливаем данные:
         long amount = 1_000_000_60;
-        boolean registered = true;
+        boolean registered = false;
         long expected = 500;
 
         // вызываем целевой метод:
@@ -54,8 +54,8 @@ public class BonusServiceTest {
 
         // подготавливаем данные:
         long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
+        boolean registered = false;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
